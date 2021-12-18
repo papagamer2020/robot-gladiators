1b7eaa79495134cd6acea3c107233ed8ea41cb46
Nate Farver <farvercreek@gmail.com>
2021-12-18 03:00:37 -0500

    moved the player money

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -9,7 +10,7 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var playerMoney = 10;
+
 
 var fight = function() {
   window.alert("Welcome to Robot Gladiators!");
