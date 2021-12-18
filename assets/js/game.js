1eeec57f1893d69bfbd52fee245e4fe8e1f24e17
Nate Farver <farvercreek@gmail.com>
2021-12-18 02:55:59 -0500

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,9 +9,15 @@ var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var playerMoney = 10;
+
 var fight = function() {
   window.alert("Welcome to Robot Gladiators!");
+
+  var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+  console.log(promptFight);
   
+  if (promptFight === "fight" || promptFight === "FIGHT"){
   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
     enemyHealth = enemyHealth - playerAttack;
 
@@ -19,7 +25,7 @@ var fight = function() {
   console.log(
     playerName + "attacked" + enemyName + "." + "now has" + enemyHealth + "health remaning"
   );
-
+  
   //check enemy's health
   if (enemyHealth <= 0) {
     window.alert(enemyName + " has died!");
@@ -27,7 +33,7 @@ var fight = function() {
   else {
     window.alert(enemyName + " still has " + enemyHealth + " health left.");
   }
-
+  
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
     playerHealth = playerHealth - enemyAttack;
   // Log a resulting message to the console so we know that it worked.
@@ -42,6 +48,25 @@ var fight = function() {
     else {
     window.alert(playerName + " still has " + playerHealth + " health left.");
   }
+}
+// if player choses to skip
+else if (promptFight === "skip" || promptFight === "SKIP"){
+    window.alert(playerName + "has chosen to skip the fight");
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    if (confrimSkip){
+        window.alert(playerName + "has decided to skip this fight. Goodbye!");
+        playerMoney = playerMoney - 2;
+    
+    }
+
+    else {
+        fight();
+    }
+}
+else {
+    window.alert("You need to choose a valid option. Try again!");
+}
 
 
 };
